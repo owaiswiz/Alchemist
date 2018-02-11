1b485cacdcbef54aae4bb8b02ed4ec28c386acbc
Saurav Kanchan <sauravnk30@gmail.com>
2018-02-11 12:18:02 +0530

    buy medicine implemented

--- Alchemy/public/javascripts/qrCodeScannerModal.js
+++ Alchemy/public/javascripts/qrCodeScannerModal.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     $('#reader').html5_qrcode(function (data) {
         // do something when code is read
         console.log(data);
+        // setTimeout()
+        Capsule.buy_medicine(parseInt(data),web3.eth.accounts[1],web3.eth.accounts[2],{from:web3.eth.accounts[2],gas:3000000});
+        $("#qrCodeScannerModal").modal("hide");
+
     },
         function (error) {
             //show read errors 
